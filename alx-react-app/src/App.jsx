f72a31433ba5a462d646cfb910bc4b9dca65f7cd
Justina Uchenna <justinefergoson@gmail.com>
2025-08-27 06:40:26 -0700

    Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
+import WelcomeMessage from './components/WelcomeMessage';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+
 
 function App() {
     return (
         <>
-        <WelcomeMessage />
             <Header />
+            <WelcomeMessage />
             <MainContent />
+            <UserProfile name="Justina" age="24" bio="Loves coding and business" />
             <Footer />
         </>
     );
