c8ef0c98e8a09622f5b6c88933f95a54af9ad7ec
Justina Uchenna <justinefergoson@gmail.com>
2025-09-06 06:01:31 -0700

    Build website with React

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer style={{ background: '#f1f1f1', padding: '10px', marginTop: '20px', textAlign: 'center' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
