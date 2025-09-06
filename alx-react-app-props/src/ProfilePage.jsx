ff89b868435b73062c0d671545925089fc87013e
Justina Uchenna <justinefergoson@gmail.com>
2025-09-06 03:22:02 -0700

    Refactor Prop Drilling to use Context API

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,11 +1,7 @@
-import { useContext } from 'react';
-import UserContext from './UserContext';
 import UserInfo from './UserInfo';
 
 function ProfilePage() {
-  const userData = useContext(UserContext);
-
-  return <UserInfo userData={userData} />;
+  return <UserInfo />;
 }
 
 export default ProfilePage;
