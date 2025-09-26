914f9278bd936a878fcb25bb2b62b34c46dd35d8
Justina Uchenna <justinefergoson@gmail.com>
2025-09-26 11:01:55 -0700

    Styling React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-600">
-      Hello, Tailwind is working! 🚀
-    </h1>
-  )
+    <div>
+      <UserProfile />
+    </div>
+  );
 }
 
 export default App
