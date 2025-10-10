56a598f5cd9a3c6be9f0d2edac25a4ec860d1b24
Justina Uchenna <justinefergoson@gmail.com>
2025-10-10 09:19:09 -0700

    Implement Todo List Component in React

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>My Todo List</h1>
+      <TodoList />
+    </div>
+    );
+}
+
+export default App;
